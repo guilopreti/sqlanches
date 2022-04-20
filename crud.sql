57e2423a584e67abad4d6d54d25bf900daf3c133
Guilherme <guilherme.llopreti@gmail.com>
2022-04-20 12:13:36 -0300

    feature: criação do banco de dados SQLanches com CRUD, selects e criação de tabelas

--- crud.sql
+++ crud.sql
@@ -4,31 +4,77 @@
 -- Criação
 
 -- 1)
-
-
+INSERT INTO
+  clientes (nome, lealdade)
+VALUES
+	('Georgia', 0);
 
 -- 2)
-
+INSERT INTO
+	pedidos (status, cliente_id)
+VALUES
+	('Recebido', 6);
 
 
 -- 3)
+INSERT INTO
+	produtos_pedidos (pedido_id, produto_id)
+VALUES
+	(6, 1),
+    (6, 2),
+    (6, 6),
+    (6, 8),
+    (6, 8);
 
 
 
 -- Leitura
 
 -- 1)
-
+SELECT
+	clientes.id,
+	clientes.nome,
+    clientes.lealdade,
+    pedidos.id,
+    pedidos.status,
+    pedidos.cliente_id,
+    produtos.id,
+    produtos.nome,
+    produtos.tipo,
+    produtos."preço",
+    produtos.pts_de_lealdade
+    FROM produtos_pedidos pp
+    JOIN produtos ON pp.produto_id = produtos.id
+    JOIN pedidos ON pedidos.id = pp.pedido_id
+    JOIN clientes ON clientes.id = pedidos.cliente_id
+    WHERE clientes.id = 6
+;
 
 
 -- Atualização
 
 -- 1)
+SELECT
+	clientes.nome,
+	SUM(pts_de_lealdade)
+	FROM produtos
+    JOIN produtos_pedidos pp ON pp.produto_id = produtos.id
+    JOIN pedidos ON pedidos.id = pp.pedido_id
+    JOIN clientes ON clientes.id = pedidos.cliente_id
+    WHERE clientes.nome = 'Georgia'
+    GROUP BY clientes.nome
+;
 
+UPDATE clientes
+	SET lealdade = 48
+    WHERE nome = 'Georgia'
+;
 
 
 -- Deleção
 
 -- 1)
-
+DELETE FROM clientes
+	WHERE nome = 'Marcelo'
+;
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,51 @@
 -- Seleções de dados
 
 -- 1)
-
+SELECT
+	pedidos.id pedido_id,
+    pedidos.status,
+    pedidos.cliente_id,
+    produtos.id produto_id,
+    produtos.nome,
+    produtos.tipo,
+    produtos."preço",
+    produtos.pts_de_lealdade pontos_de_lealdade_do_produto
+    FROM pedidos
+    JOIN produtos_pedidos pp ON pp.pedido_id = pedidos.id
+    JOIN produtos ON produtos.id = pp.produto_id
+;
 -- 2)
-
+SELECT
+	pedidos.id
+    FROM pedidos
+    JOIN produtos_pedidos pp ON pp.pedido_id = pedidos.id
+	JOIN produtos ON produtos.id = pp.produto_id
+    WHERE produtos.nome = 'Fritas'
+;
 -- 3)
-
+SELECT
+	clientes.nome gostam_de_fritas
+    FROM pedidos
+    JOIN produtos_pedidos pp ON pp.pedido_id = pedidos.id
+	JOIN produtos ON produtos.id = pp.produto_id
+    JOIN clientes ON clientes.id = pedidos.cliente_id
+    WHERE produtos.nome = 'Fritas'
+;
 -- 4)
-
+SELECT
+	SUM(produtos."preço")
+    FROM produtos
+    JOIN produtos_pedidos pp ON pp.produto_id = produtos.id
+    JOIN pedidos ON pedidos.id = pp.pedido_id
+    JOIN clientes ON clientes.id = pedidos.cliente_id
+    WHERE clientes.nome = 'Laura'
+;
 -- 5)
+SELECT
+	produtos.nome,
+	COUNT(produto_id)
+    FROM produtos
+    JOIN produtos_pedidos pp ON pp.produto_id = produtos.id
+	GROUP BY produtos.nome
+    ORDER BY produtos.nome
+;
